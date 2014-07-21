ac54e260d96ce2754ea65da70339b162e83d0052
Dan Cochran <dcsquared.13@gmail.com>
2014-07-20 19:18:10 -0700

    added some more basic functionality.  Basic straight line predict for single id.

--- src/Final/hexbug_predict.py
+++ src/Final/hexbug_predict.py
@@ -11,7 +11,7 @@ def distance_between(point1, point2):
 
 def get_input(filename):
     #Read in the input file and create array of the data points.
-    p = re.compile('[-0-9]+')
+    p = re.compile('[-0-9]+')  #this should find the points on each line of the input (numeric + or -).  All integers.
     ia = []
     f = open(filename, 'r')
     for line in f:
@@ -54,6 +54,29 @@ def process(in_a):
         m.append(dict)
     return np.array(m), min(allX), max(allX), min(allY), max(allY)
 
+def simple_next_move(current, distance, heading):
+    #Simple next move.  Take current move, avg distance, and heading to predict next spot.
+    x = current[0] + distance * cos(heading)
+    y = current[1] + distance * sin(heading)     
+    return (int(x), int(y))
+    
+def get_distance_mean(map):
+    #returns the mean distance based on the input map.
+    dblist = []
+    for i in range(len(map)):
+        if map[i]["db"] != None:
+            dblist.append(map[i]["db"])   
+    return np.mean(dblist) 
+
+def predict_next(mapfile, d, id=all):
+    pm = []
+    if id != all:
+        cur_point = mapfile[id]
+        x,y = simple_next_move(cur_point["cur_point"], d, cur_point["angle"])
+        hm = {"coord" : [x,y]}
+        pm.append(hm)
+        
+    return pm
 #Get the input array
 input_array = get_input('training_video1-centroid_data')
 
@@ -63,7 +86,14 @@ print "Min X:  ", minX
 print "Max X:  ", maxX
 print "Min Y:  ", minY
 print "Max Y:  ", maxY
+print "Map Length:  ", len(map)
+  
+db_mean = get_distance_mean(map)
+print "Distance Between Mean: ", db_mean  
 
-print len(map)
+#This is a test
+predict_map = predict_next(map, db_mean, 25008)
+
+print predict_map
 
 write_map_to_file(map)
